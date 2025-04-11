e229ea638a2def6a0d1925d0c8d94919bc235f7f
Charlie Braun <Brauncw@colostate.edu>
2025-04-11 12:01:07 -0600

    Create Table 1 Fixes #5

---
+++ src/app/pages/table1/table1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table1',
+  imports: [],
+  templateUrl: './table1.component.html',
+  styleUrl: './table1.component.css'
+})
+export class Table1Component {
+
+}
