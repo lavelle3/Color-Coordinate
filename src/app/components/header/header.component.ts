ac41d666331044e772ab41fd80872a057e03fbec
Charlie Braun <Brauncw@colostate.edu>
2025-04-11 11:30:08 -0600

    Create About Page, bugfix. Fixes #2

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router'
-import { RouterLinkActive } from '@angular/router';
+import { Router } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrls: ['./header.component.css'] 
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
 
+  navigateIfNeeded(path: string, event: Event) {
+    if (this.router.url === path) {
+      event.preventDefault(); 
+    }
+  }
 }
--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -11,24 +11,30 @@ import { NgOptimizedImage } from '@angular/common';
 export class AboutComponent {
   teamMembers = [
     {
-      name: 'Devin __',
+      name: 'Devin',
       bio: "",
-      profileImage: ''
+      profileImage: '/fatfatmillycat.jpg' 
+      // profile image is a placeholder, NgSrc breaks without a link
+      // put your images in Color-Coordinate/public
     },
     {
       name: 'Edgar',
       bio: "",
-      profileImage: ''
+      profileImage: '/fatfatmillycat.jpg'
+      // profile image is a placeholder, NgSrc breaks without a link
+      // put your images in Color-Coordinate/public
     },
     {
       name: 'Nick',
       bio: "",
-      profileImage: ''
+      profileImage: '/fatfatmillycat.jpg'
+      // profile image is a placeholder, NgSrc breaks without a link
+      // put your images in Color-Coordinate/public
     },
     {
       name: 'Charlie Braun',
       bio: "Hey! I'm Charlie Braun, a Business major, who took Computer Science as a side interest minor. I felt it would be a fun way to fill my AUCC credits, and have really enjoyed completing it.",
-      profileImage: ''
+      profileImage: '/CBraunHeadshotSquare.jpg'
     }
   ];
 }
